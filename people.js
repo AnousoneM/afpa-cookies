ba30b986d9e24c071ce11f1b0c5ae71c28b030b1
AnousoneM <anousone.mounivongs@gmail.com>
2023-10-25 21:24:23 +0200

    mise en place du repo

---
+++ people.js
@@ -0,0 +1,20 @@
+const people = [
+    {
+        nom : 'Riri',
+        race : 'Canard',
+        couleur : 'Rouge',
+        food : 'Hamburger'
+    },
+    {
+        nom : 'Fifi',
+        race : 'Canard',
+        couleur : 'Vert',
+        food : 'Raclette'
+    },
+    {
+        nom : 'Loulou',
+        race : 'Canard',
+        couleur : 'Bleu',
+        food : 'Pizza'
+    }
+]
